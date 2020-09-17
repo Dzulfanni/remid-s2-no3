79e064d5f61cf77a61bd177b2e6cb3c340959237
Dzulfanni <belajarayok866@gmail.com>
2020-09-17 09:49:50 +0700

    first commit

---
+++ no3.js
@@ -0,0 +1,9 @@
+var panjang = 10;
+var lebar = 5;
+var luas = function luas(panjang, lebar){
+    return (panjang*lebar);
+    
+}
+document.write('<br>'+'Panjang= '+panjang+'</br>');
+document.write('<br>'+'lebar= '+lebar+'</br>');
+document.write('<br>'+'luas persegi panjang = '+luas(10, 5)+'</br>');
